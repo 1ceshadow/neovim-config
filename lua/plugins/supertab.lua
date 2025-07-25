c87525b983ff9cbe3c293c2cf27c8c6b45657391
1ceshadow <fei_er_ma_si@163.com>
2025-07-25 18:44:33 +0800

    修改super-tab

--- lua/plugins/supertab.lua
+++ lua/plugins/supertab.lua
@@ -1,11 +1,19 @@
--- 修改blink.cmp的默认按键
 return {
-  "saghen/blink.cmp",
-  opts = function(_, opts)
-    opts.keymap = {
-      preset = "default",
-      ["<Tab>"] = { "select_and_accept" },
-    }
-    return opts
-  end,
+  "Saghen/blink.cmp",
+  opts = {
+    keymap = {
+      preset = "super-tab",
+      ["<Tab>"] = {
+        function(cmp)
+          if cmp.snippet_active() then
+            return cmp.accept()
+          else
+            return cmp.select_and_accept()
+          end
+        end,
+        "snippet_forward",
+        "fallback",
+      },
+    },
+  },
 }
